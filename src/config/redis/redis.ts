8e5d5b79c7c8c0612df80b26e8bdfd12eb55a399
daovinhloc <locdvtcs21019@fpt.edu.vn>
2025-04-15 22:37:57 +0700

    fix: remove redis

--- src/config/redis/redis.ts
+++ src/config/redis/redis.ts
@@ -1,10 +1,10 @@
-import { createClient } from '@redis/client';
+// import { createClient } from '@redis/client';
 
-const redis = createClient({
-  url: process.env.REDIS_URL || 'redis://localhost:6379',
-});
+// const redis = createClient({
+//   url: process.env.REDIS_URL || 'redis://localhost:6379',
+// });
 
-redis.on('error', (err) => console.error('Redis Client Error', err));
-await redis.connect();
+// redis.on('error', (err) => console.error('Redis Client Error', err));
+// await redis.connect();
 
-export default redis;
+// export default redis;
--- src/pages/api/auth/verifyRegister.ts
+++ src/pages/api/auth/verifyRegister.ts
@@ -1,36 +1,36 @@
-import { UserUSeCaseInstance } from '@/features/auth/application/use-cases/userUseCase';
-import { errorHandler, NotFoundError, ValidationError } from '@/shared/lib/responseUtils/errors';
-import redis from '@/config/redis/redis';
-import { NextApiRequest, NextApiResponse } from 'next';
+// import { UserUSeCaseInstance } from '@/features/auth/application/use-cases/userUseCase';
+// import { errorHandler, NotFoundError, ValidationError } from '@/shared/lib/responseUtils/errors';
+// import redis from '@/config/redis/redis';
+// import { NextApiRequest, NextApiResponse } from 'next';
 
-async function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
-  }
-  const { email, otp } = req.body;
+// async function handler(req: NextApiRequest, res: NextApiResponse) {
+//   if (req.method !== 'POST') {
+//     return res.status(405).json({ error: 'Method not allowed' });
+//   }
+//   const { email, otp } = req.body;
 
-  const storedOTP = await redis.get(`otp:${email}`);
+//   const storedOTP = await redis.get(`otp:${email}`);
 
-  if (!storedOTP || storedOTP !== otp) {
-    throw new ValidationError('OTP không hợp lệ');
-  }
+//   if (!storedOTP || storedOTP !== otp) {
+//     throw new ValidationError('OTP không hợp lệ');
+//   }
 
-  const user = await UserUSeCaseInstance.verifyEmail(email);
-  if (!user) {
-    throw new NotFoundError('Tài khoản không tồn tại');
-  }
+//   const user = await UserUSeCaseInstance.verifyEmail(email);
+//   if (!user) {
+//     throw new NotFoundError('Tài khoản không tồn tại');
+//   }
 
-  if (user.emailVerified) {
-    throw new ValidationError('Email đã được xác thực');
-  }
+//   if (user.emailVerified) {
+//     throw new ValidationError('Email đã được xác thực');
+//   }
 
-  await UserUSeCaseInstance.verifyUser(email);
-  await redis.del(`otp:${email}`);
+//   await UserUSeCaseInstance.verifyUser(email);
+//   await redis.del(`otp:${email}`);
 
-  res.status(200).json({ message: 'Tài khoản đã xác thực thành công ! Vui lòng đăng nhập' });
-}
+//   res.status(200).json({ message: 'Tài khoản đã xác thực thành công ! Vui lòng đăng nhập' });
+// }
 
-const errorHandlerWrapper = (req: NextApiRequest, res: NextApiResponse) =>
-  errorHandler(handler, req, res);
+// const errorHandlerWrapper = (req: NextApiRequest, res: NextApiResponse) =>
+//   errorHandler(handler, req, res);
 
-export default errorHandlerWrapper;
+// export default errorHandlerWrapper;
